5369df2271039656232a824c6bfd4048d1ef417d
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2024-10-15 02:40:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/DeveloperBoy-A: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.git/DeveloperBoy-A/Newmovies_BOT-
+git clone -b master https://github.com/DeveloperBoy-A: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.git/DeveloperBoy-A/Newmovies_BOT-.git
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
